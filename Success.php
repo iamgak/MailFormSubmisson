6388a4ed84b5a82e37be1eb87da0f97b7daa0032
Gufran Ahmed Khan <91026137+iamgak@users.noreply.github.com>
2023-08-14 04:21:15 +0530

    Create Success.php

---
+++ Success.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form Submission Confirmation</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .container{
+        	display:flex;
+        	flex-direction: column;
+        	justify-content: center;
+            align-items: center;
+        }
+        .message {
+            font-size: 24px;
+            text-align: center;
+
+        }
+        
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body><div class="container">
+    <div class="message">
+        <p>Your form has been successfully submitted.
+        Badhai ho!!</p>
+    </div>
+    <a class="button" href="index.html">Go Back to Form</a>
+</div>
+</body>
+</html>
